8c77d5b8d5de6ba3a50fe7caca4067db70052865
Jared7Lu <109761254+Jared7Lu@users.noreply.github.com>
2023-04-27 19:48:32 -0700

    first sql table

---
+++ server/migrations/20230428023918-create-account.js
@@ -0,0 +1,21 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('accounts', {
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('accounts');
+  }
+};
