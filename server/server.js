01381c2f144aa221c496c72162f69b2c8368525f
Jared7Lu <109761254+Jared7Lu@users.noreply.github.com>
2023-04-28 20:18:45 -0700

    server routes outline

--- server/server.js
+++ server/server.js
@@ -12,11 +12,37 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //Routes
-
 //create account 
-// app.post(/)
-// //find account
-// app.get()
+app.post("/create", (req, res) => {
+    db.query(
+        "INSERT INTO useraccounts (user_name, pass_word) VALUES( ?, ?)", [user_name, pass_word],
+        (err, result) => {
+            console.log(err);
+        }
+    )
+})
+
+// login into account
+app.get("/useraccounts/:id", async (req, res) => {
+    db.query(
+        "SELECT * FROM useraccounts WHERE user_name = ? AND pass_word = ?", [username, password],
+        (err, result) => {
+            if (err) {
+                res.send({ err: err});
+            }
+
+            if (result) {
+                res.send(result);
+            } else {
+                res.send({message: "Wrong username/password"})
+            }
+        }
+    )
+})
+
+//delete account 
+
+//update account
 
 app.listen(3090, () => {
     console.log("3090 server good")
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import { useState } from "react"
 
 //pages
